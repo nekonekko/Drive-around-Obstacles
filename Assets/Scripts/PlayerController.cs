3f4870c30c4537d0cbedc060d99c789225fd97d5
nekonekko <momoichigodayo+github@gmail.com>
2023-09-05 23:11:02 +0900

    feat: :sparkles: rotate vehicle instead of slide

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,14 +13,16 @@ public class PlayerController : MonoBehaviour
     public float speed = 20;
     public float turnSpeed;
     public float horizontalInput;
+    public float forwardInput;
 
     // Update is called once per frame
     void Update()
     {
         // Move the vehicle forward
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
