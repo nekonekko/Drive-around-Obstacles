b8a94d54b55be5b2bf9861da31925d8f362bd645
nekonekko <momoichigodayo+github@gmail.com>
2023-09-07 20:23:40 +0900

    feat: :sparkles: toggle camera position with space key

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 behindOffset = new Vector3(0, 5, -7);
+    private Vector3 driverSeatOffset = new Vector3(0, 3, 2);
+    private int spaceInputCount = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,20 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        if (Input.GetKeyDown("space"))
+        {
+            spaceInputCount++;
+        }
+
+        if (spaceInputCount % 2 == 0)
+        {
+            transform.position = player.transform.position + behindOffset;
+            transform.rotation = Quaternion.Euler(20, 0, 0);
+        }
+        else
+        {
+            transform.position = player.transform.position + driverSeatOffset;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
